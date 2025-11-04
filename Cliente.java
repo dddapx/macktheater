7af6f7c994e997d584b132adbcf232036e3ac495
dddapx <davi.bfdepaiva@gmail.com>
2025-11-04 01:01:29 +0000

    macktheater

---
+++ Cliente.java
@@ -0,0 +1,14 @@
+public class Cliente {
+    //lista de clientes(?)
+    private String nome;
+    private String cpf;
+
+    public Cliente (String nome, String cpf){
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public void adicionaPedido(Pedido pedido){
+        
+    }
+}
---
+++ Pedido.java
@@ -0,0 +1,13 @@
+public class Pedido {
+    public Pedido(){
+
+    }
+
+    public void adicionaEntrada(Entrada entrada){
+
+    }
+
+    public double calculaValorTotal(){
+        
+    }
+}
